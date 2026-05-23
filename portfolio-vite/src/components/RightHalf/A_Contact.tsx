93a3c7f33c3556aa28d0c36c139c9655d723ec57
Avinash Sorab <avinashsp93@gmail.com>
2026-05-23 14:26:07 -0400

    add animation for contact on hover

--- portfolio-vite/src/components/RightHalf/A_Contact.tsx
+++ portfolio-vite/src/components/RightHalf/A_Contact.tsx
@@ -6,20 +6,32 @@ function Contact() {
       contact: { location, phone, email, resume },
     },
   } = useData();
+
   return (
     <div className="contact row">
       <ul className="contact-list">
-        <li id="location" className="h6 text-uppercase">
-          {location}
+        <li id="location" className="h6 text-uppercase flip-item">
+          <div className="flip-inner">
+            <div className="flip-front">Location</div>
+            <div className="flip-back">{location}</div>
+          </div>
         </li>
         <li id="phone" className="h6 text-uppercase">
-          {phone}
+          <div className="flip-inner">
+            <div className="flip-front">Phone</div>
+            <div className="flip-back">{phone}</div>
+          </div>
         </li>
         <li id="email" className="h6 text-uppercase">
-          {email}
+          <div className="flip-inner">
+            <div className="flip-front">Email</div>
+            <div className="flip-back">{email}</div>
+          </div>
         </li>
         <li id="resume" className="h6 text-uppercase">
-          {resume}
+          <a href={resume} target="_blank" rel="noopener noreferrer">
+            Resume
+          </a>
         </li>
       </ul>
     </div>
