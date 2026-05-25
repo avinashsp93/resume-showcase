e28b64b9738ab08d0618a4e951f24b997c668857
Avinash Sorab <avinashsp93@gmail.com>
2026-05-24 20:45:37 -0400

    add google analytics

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,14 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./index.css";
 import App from "./App.tsx";
 import { DataProvider } from "./contexts/DataContext";
+import ReactGA from "react-ga4";
+
+ReactGA.initialize(import.meta.env.VITE_GA_ID);
+
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname,
+});
 
 const rootElement = document.getElementById("root");
 
